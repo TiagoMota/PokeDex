7fce6d99bf5b405af8f61d6b6a8e0189484e9198
Tiago Mota <neozflux@gmail.com>
2016-03-09 23:58:22 +0000

    Updated PokeDexCell to display loading, error and content views. Loading pokemons per cell righ now, need to change this logic to load pages of 20 and simulate an infinite scrolling on the collection view

--- PokeDex/View/PokeDexCell.swift
+++ PokeDex/View/PokeDexCell.swift
@@ -13,20 +13,55 @@ class PokeDexCell: UICollectionViewCell {
 
     @IBOutlet weak var _thumbImg : UIImageView!
     @IBOutlet weak var _nameLabel : UILabel!
+    @IBOutlet weak var _progressView : UIActivityIndicatorView!
     
     private var _pokemon: Pokemon!
+    
+    var pokemon : Pokemon {
+        return self._pokemon
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 5.0
+    }
+    
     
     func configureCell (pokemon: Pokemon) {
         self._pokemon = pokemon
         
+        // set pokemon name
+        self._nameLabel.text = pokemon.name.capitalizedString
+        
+        // set pokemon image thumbnail by loading the image with Nuke
         let url = "\(Constants.URL_POKEMON_IMG)\( self._pokemon.pokeId).png"
         var request = ImageRequest(URLRequest: NSURLRequest(URL: NSURL(string: url)!))
         request.priority = NSURLSessionTaskPriorityHigh
         
         Nuke.taskWith(request) {
-            // - Image is resized to fill target size
-            // - Blur filter is applied
+            // hide progress view
+            self._progressView.stopAnimating()
+            
+            // set and show pokemon image
             self._thumbImg.image = $0.image
+            self._thumbImg.hidden = false
+            
             }.resume()
+    }
+    
+    
+    func showError(error: NSError) {
+        self._nameLabel.text = "ERROR!"
+        print("Error loading pokemon: \(error.code) - \(error.description)")
+    }
+    
+    func showLoading() {
+        // hide image view
+        self._thumbImg.hidden = true
+        
+        // show progress view
+        self._progressView.hidesWhenStopped = true
+        self._progressView.startAnimating()
     }
 }
--- PokeDex/ViewController.swift
+++ PokeDex/ViewController.swift
@@ -9,18 +9,64 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate,
+UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var _collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        let URL = "\(Constants.URL_POKEMON)1"
-        Alamofire.request(.GET, URL).responseObject { (response: Response<Pokemon, NSError>) -> Void in
+        self._collectionView.delegate = self
+        self._collectionView.dataSource = self
+    }
+
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeDexCell", forIndexPath: indexPath) as? PokeDexCell {
             
-            let pokemon = response.result.value
-            print(pokemon?.name)
+            cell.showLoading()
+            
+            let URL = "\(Constants.URL_POKEMON)\(indexPath.row + 1)"
+            debugPrint("Cell request URL -> \(URL)")
+            Alamofire.request(.GET, URL).responseObject { (response: Response<Pokemon, NSError>) -> Void in
+                
+                if let error = response.result.error {
+                    cell.showError(error)
+                } else {
+                    cell.configureCell(response.result.value!)
+                }
+            }
+            
+            return cell
+        } else {
+            // in case of error return empty cell
+            return UICollectionViewCell()
         }
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        // when item is selected
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        
+        // just one section for now
+        return 1
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        return CGSize(width: 110, height: 110)
     }
 }
 
